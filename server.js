dc930d9284e5e569cb2e21a9efa8c9de5f9b3b8f
Fabricio Murillo <fabriciomurillo24@gmail.com>
2020-12-05 00:55:28 -0600

    Instancias

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const car = require("./models/Carro.js");
+const bomb = require("./models/Bomba");
+const repository = require("./models/Almacen");
 
 let carro = new car.Carro();
 console.log(JSON.stringify(carro));
@@ -11,6 +13,15 @@ carro = new car.Carro();
 console.log(JSON.stringify(carro));
 carro = new car.Carro();
 console.log(JSON.stringify(carro));
+
+let bomba = new bomb.Bomba(45);
+console.log("Bomba", bomba.getCaudal);
+
+let almacen = new repository.Almacen(100, 29);
+console.log("almacen.getCapacidad", almacen.getCapacidad);
+console.log("almacen .getNivel", almacen.getNivel);
+almacen.setNivel = 4;
+console.log("almacen .getNivel", almacen.getNivel);
 
 app.use("/", express.static("front"));
 
