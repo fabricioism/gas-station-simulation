7ed12b7c9a1d586d7ab18bf11c834102c24924e1
josue silva <josuemarkcs@gmail.com>
2020-12-08 11:20:38 -0600

    Funcionalidad del tiempo,cantidad de bombas y obtencion de datos del formulario

--- front/js/index.js
+++ front/js/index.js
@@ -5,6 +5,10 @@ const btnIniciar = document.getElementById("btn-iniciar");
 btnIniciar.addEventListener("click", function (e) {
   console.log("Boton iniciar");
   socket.emit("iniciar", { data: "prueba" });
+  
+  obtenerInformacion()
+  gestionBombas(obtenerInformacion());
+
 });
 
 // MANEJO DE RESPUESTAS SOCKETS
@@ -22,3 +26,109 @@ socket.on("actualizacion", function (data) {
 });
 
 // FIN MANEJO DE RESPUESTAS SOCKETS
+
+// Gestion de la informacion
+
+let temporizador = document.getElementById('temporizador');
+let resetear = document.getElementById('resetear');
+let botones = document.getElementById('botones');
+let tiempo = 0, intervale = 0;
+let verificador = false;
+
+resetear.disabled = 'disabled';
+
+init()
+
+function init(){
+  btnIniciar.addEventListener('click',iniciarCronometro)
+  resetear.addEventListener('click',resetearCronometro)
+}
+
+function iniciarCronometro(){
+  if(verificador == false){
+    intervalo = setInterval(function(){
+      tiempo += 0.01;
+      temporizador.innerHTML = `${tiempo.toFixed(2)}s`;
+    },10);
+    verificador = true;
+    btnIniciar.innerHTML = ''
+    btnIniciar.className = 'btn btn-danger'
+    btnIniciar.innerHTML = ` 
+       <b>Pausar</b>
+     `;
+
+     resetear.disabled = false;
+
+  }else{
+    verificador = false;
+    clearInterval(intervalo)
+     btnIniciar.innerHTML = ''
+     btnIniciar.className = 'btn btn-info'
+     btnIniciar.innerHTML = `
+       <b>Iniciar</b>
+     `;
+  }
+}
+
+function resetearCronometro(){
+  verificador = false;
+  tiempo = 0.00;
+  temporizador.innerHTML = `${tiempo}.00s`
+  clearInterval(intervalo);
+}
+
+function obtenerInformacion(){
+  let Entradas = {
+    cantidad_bombas:document.getElementById("bombas").value,
+    cantidad_diesel:document.getElementById("diesel").value,
+    cantidad_gasolina:document.getElementById("gasolina").value,
+    flujo_bombas:document.getElementById("flujo").value,
+    velocidad_simulacion:document.getElementById("velocidad").value
+  }
+
+  console.log(Entradas)
+
+  return Entradas;
+}
+
+function gestionBombas(data){
+  let contenedor_bombas = document.getElementById("contenedor_bombas");
+  contenedor_bombas.innerHTML = ''
+  
+  for (let i = 0; i < data.cantidad_bombas; i++) {
+    contenedor_bombas.innerHTML += `
+        <div class="col-6 bomba">
+        <h5>BOMBA # ${i+1}</h5>
+        <table class="table table-borderless">
+          <thead>
+            <tr>
+              <th scope="col" class="titulo-tabla">Atentidos</th>
+              <th scope="col" class="titulo-tabla">Diesel</th>
+              <th scope="col" class="titulo-tabla">Gasolina</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="cantidad-autos">50</td>
+              <td class="cantidad-autos" style="color: #ffca2b">
+                24
+              </td>
+              <td class="cantidad-autos" style="color: #dc3545">
+                26
+              </td>
+            </tr>
+            <tr>
+              <td class="cantidad-litros">150 litros</td>
+              <td class="cantidad-litros" style="color: #ffca2b">
+                75 litros
+              </td>
+              <td class="cantidad-litros" style="color: #dc3545">
+                75 litros
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    `
+  }
+}
